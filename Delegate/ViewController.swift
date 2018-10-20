22457315c9b84a0ac99bde08df26df660ac89a64
Hoang Ngoc Sinh <h.sinh@bravesoft.co.jp>
2018-10-21 00:23:54 +0900

    Initial Commit

---
+++ Delegate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate
+//
+//  Created by ホアンゴックシン on 2018/10/21.
+//  Copyright © 2018 vnguider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
